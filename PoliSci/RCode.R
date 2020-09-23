e6d97d27515bc866c7f14a92ca9ee14854a185ea
DragonflyStats <kobriendublin@gmail.com>
2020-09-23 14:09:58 +0100

    Create RCode.R

---
+++ PoliSci/RCode.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(magrittr)
+
+myData <- read.csv("https://raw.githubusercontent.com/DragonflyStats/Projects/master/PoliSci/Facebook.csv")
+myData <- myData %>% arrange(Name,Date)
+
+Update <- myData %>% arrange(Name,desc(Date)) %>% 
+          group_by(Name) %>% 
+          slice(1) %>%
+          arrange(Date)
+
+
