3f48234168bbe0814b547d8a334c013b04258971
DragonflyStats <kobriendublin@gmail.com>
2020-10-03 11:06:59 +0100

    Update RCode.R

--- PoliSci/RCode.R
+++ PoliSci/RCode.R
@@ -1,12 +1,15 @@
 library(dplyr)
 library(magrittr)
+library(readr)
 
-myData <- read.csv("https://raw.githubusercontent.com/DragonflyStats/Projects/master/PoliSci/Facebook.csv")
+myData <- read_csv("https://raw.githubusercontent.com/DragonflyStats/Projects/master/PoliSci/Facebook.csv")
 myData <- myData %>% arrange(Name,Date)
 
 Update <- myData %>% arrange(Name,desc(Date)) %>% 
           group_by(Name) %>% 
           slice(1) %>%
           arrange(Date)
+
+Update %>% View()
 
 write.csv(myData, "Facebook.csv",row.names=FALSE)
